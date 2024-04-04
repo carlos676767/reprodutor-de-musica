30b2d4045789f976ed00d0af3835e409c7e50e21
carlos676767 <carlitobr730@gmail.com>
2024-04-04 07:35:31 -0700

    valores escodendno ok.

--- projeto/src/main.ts
+++ projeto/src/main.ts
@@ -5,24 +5,32 @@ const daPlayAudio = () => {
 }
 
 const botaoPlay = document.getElementById("play") as HTMLButtonElement
+const pause = document.getElementById("pause") as HTMLButtonElement
+const playsOfEblock = (valor1: HTMLButtonElement, valor2:  HTMLButtonElement) => {
+ valor1.style.display = "none"
+ valor2.style.display = "block"
+}
 
 botaoPlay.addEventListener("click", () => {
   daPlayAudio()
+  playsOfEblock(botaoPlay, pause)
 })
 
+
+
+
 const inputVolume = document.getElementById("baixarVolume") as HTMLInputElement
-
-
-
 
 const baixarVolumeFuncao = () => {
   const obterVolumes = parseInt(inputVolume.value)/100
   audio.volume = obterVolumes
 }
 
+
 inputVolume.addEventListener("input", () => {
   baixarVolumeFuncao()
 })
+
 
 
 
