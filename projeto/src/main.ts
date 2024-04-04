3aa55a14ff4fe4d74c18659d9ac0d693d5bd38ab
carlos676767 <carlitobr730@gmail.com>
2024-04-04 16:40:11 -0700

    botao de like ok.

--- projeto/src/main.ts
+++ projeto/src/main.ts
@@ -1,3 +1,22 @@
+const musicasGeradas = async () => {
+  const token = "BQA3cmSoYMWLQAg_com4plDvG3q5FcfsY4SvBywPWHbp89XPhalaZ3mK7B27rCk3lvXtlKAECEc9RE3jGi6wYOVO5UEbXYelE61-95EjK8yffeCCckUz2amhKcc5FZ-RmhM5iXmbJWZWtwod_df7O5b-VKj7EeVf9pue9OsXwOPm_c5GcrTYXgOumQRkpobKnOnX17qjUfptkyLVYR1C1F2UAkeZqc0qjWYgnbdksqE0f2Nb1B81egxz4JuMFViwKpIdkZDcfkGraOsDrMAN"
+  try {
+    const data = await fetch(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=enter+sandman&api_key=0e6a2fc1d401d2165d6a012ea73231e3&format=json    `, {
+      method: "GET",
+   
+    })
+    const response = await data.json()
+    console.log(response);
+
+  } catch (erro) {
+    console.log(erro);
+    
+  }
+}
+
+musicasGeradas()
+
+
 const audio = new Audio("m.mp3")
 
 const daPlayAudio = () => {
@@ -13,6 +32,7 @@ const playsOfEblock = (valor1: HTMLElement, valor2: HTMLElement): void => {
 
 botaoPlay.addEventListener("click", () => {
   daPlayAudio()
+  console.log(audio.duration);
   playsOfEblock(botaoPlay, pause)
 })
 
@@ -51,7 +71,7 @@ audioMutado.addEventListener("click", () => {
 
 
 const botaoDesmutado = document.getElementById("desmutado") as HTMLElement
-console.log(botaoDesmutado);
+
 
 
 const desmultarAudio = () => {
@@ -81,4 +101,15 @@ const musicaEmLoppInfinito = () => {
 
 botaoRepitir.addEventListener("click", () => {
   musicaEmLoppInfinito()
+})
+
+const like = document.querySelector(".fa-heart") as HTMLElement
+
+
+like.addEventListener("click", () => {
+  like.classList.add("coracaoVermelho")
+  setTimeout(() => {
+    like.classList.remove("coracaoVermelho")
+    like.classList.add("coracaoWhite")
+  }, 1000);
 })
